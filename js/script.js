e970ac2797d4e99047693d6505f39d8b0ae8659d
Vovka <batilado88@gmail.com>
2023-01-13 01:35:19 +0400

    19#practice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const q1 = prompt('Один из последних просмотренных фильмов?', '');
+const q2 = +prompt('На сколько оцените его?', '');
+const q3 = prompt('Один из последних просмотренных фильмов?', '');
+const q4 = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[q1] = q2;
+personalMovieDB.movies[q3] = q4;
+
+console.log(personalMovieDB);
