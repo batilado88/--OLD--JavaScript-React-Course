7a05d259aa5cb1ca127ed57ea451031f1e181182
Vovka <batilado88@gmail.com>
2023-01-15 03:27:51 +0400

    #24 Lesson

--- js/script.js
+++ js/script.js
@@ -10,12 +10,50 @@ const personalMovieDB = {
     privat: false
 };
 
-const q1 = prompt('Один из последних просмотренных фильмов?', '');
-const q2 = +prompt('На сколько оцените его?', '');
-const q3 = prompt('Один из последних просмотренных фильмов?', '');
-const q4 = +prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const q1 = prompt('Один из последних просмотренных фильмов?', '');
+    const q2 = prompt('На сколько оцените его?', '');
+    if (q1 != '' && q1 != null && q2 != '' && q2 != null && q1.length < 50) {
+        personalMovieDB.movies[q1] = q2;
+    } else {
+        i--;
+    }
+}
 
-personalMovieDB.movies[q1] = q2;
-personalMovieDB.movies[q3] = q4;
+// Перепишем цикл другим способом, через while
+
+// let i = 0;
+// // while (i < 2) {
+// //     const q1 = prompt('Один из последних просмотренных фильмов?', '');
+// //     const q2 = prompt('На сколько оцените его?', '');
+// //     if (q1 != '' && q1 != null && q2 != '' && q2 != null && q1.length < 50) {
+// //         personalMovieDB.movies[q1] = q2;
+// //         i++;
+// //     }
+// // }
+
+// Перепишем цикл еще одним способом, через do while
+
+// let i = 0;
+// do {
+//     const q1 = prompt('Один из последних просмотренных фильмов?', '');
+//     const q2 = prompt('На сколько оцените его?', '');
+//     if (q1 != '' && q1 != null && q2 != '' && q2 != null && q1.length < 50) {
+//         personalMovieDB.movies[q1] = q2;
+//         i++;
+//     }
+// } while (i < 2);
+
+
+if (personalMovieDB.count <= 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
+
--- js/test.js
+++ js/test.js
@@ -1,49 +1,49 @@
 'use strict';
 
-if (0) {
-    console.log('Ok!');
-} else {
-    console.log('Error');
-}
+// if (0) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error');
+// }
 
-let num = '100';
+// let num = '100';
 
-switch (num) {
-    case 50: 
-        console.log('Мало');
-        break;
-    case 75: 
-        console.log('Мало');
-        break;
-    case 100: 
-        console.log('Оно!!');
-        break;
-    case 200: 
-        console.log('Много');
-        break;
-    default: 
-        console.log('Ну хз');
-        break;
-}
+// switch (num) {
+//     case 50: 
+//         console.log('Мало');
+//         break;
+//     case 75: 
+//         console.log('Мало');
+//         break;
+//     case 100: 
+//         console.log('Оно!!');
+//         break;
+//     case 200: 
+//         console.log('Много');
+//         break;
+//     default: 
+//         console.log('Ну хз');
+//         break;
+// }
 
-console.log(!3);
+// console.log(!3);
 
 
-console.log( NaN || 2 || undefined );
+// console.log( NaN || 2 || undefined );
  
-console.log( NaN && 2 && undefined );
+// console.log( NaN && 2 && undefined );
  
-console.log( 1 && 2 && 3 );
+// console.log( 1 && 2 && 3 );
  
-console.log( !1 && 2 || !3 );
+// console.log( !1 && 2 || !3 );
  
-console.log( 25 || null && !3 );
+// console.log( 25 || null && !3 );
  
-console.log( NaN || null || !3 || undefined || 5);
+// console.log( NaN || null || !3 || undefined || 5);
  
-console.log( NaN || null && !3 && undefined || 5);
+// console.log( NaN || null && !3 && undefined || 5);
  
-console.log( 5 === 5 && 3 > 1 || 5);
+// console.log( 5 === 5 && 3 > 1 || 5);
 
 
 
@@ -83,12 +83,12 @@ console.log( 5 === 5 && 3 > 1 || 5);
 
 
 
-for (let i = 0; i < 3; i++) {
-    console.log(i);
-    for (let j = 0; j < 3; j++) {
-        console.log(j);
-    }
-}
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j);
+//     }
+// }
 
 // 0 0 1 2 1 0 1 2 2 0 1 2
 
@@ -110,31 +110,92 @@ for (let i = 0; i < 3; i++) {
 
 
 
-let result = '';
-let count = 7;
+// let result = '';
+// let count = 7;
 
-for (let i = 1; i < count; i++) {
-    // result = '*';
-    for (let j = 0; j < i; j++) {
-        result += '*';
-    }
-    result += '\n'; 
-}
+// for (let i = 1; i < count; i++) {
+//     // result = '*';
+//     for (let j = 0; j < i; j++) {
+//         result += '*';
+//     }
+//     result += '\n'; 
+// }
 
-console.log(result);
-
-
+// console.log(result);
 
 
 
-metka: for (let i = 0; i < 3; i++) {
-    console.log(`First level: ${i}`);
-    metka2: for (let j = 0; j < 3; j++) {
-        console.log(`Second level: ${j}`);
-        for (let k = 0; k < 3; k++) {
-            if (k === 2) {continue metka2;}
-            console.log(`Third level: ${k}`);
-        }
-    }
-}
+
+
+// metka: for (let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+//     metka2: for (let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 3; k++) {
+//             if (k === 2) {continue metka2;}
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+// }
+
+
+
+// const arr = [];
+
+// for (let i = 5, j = 0; i <= 10; i++) {
+//     arr[j++] = i;
+//     console.log(arr);
+// }
+
+
+
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+// let figure = '';
+
+// for (let i = 0; i < 6; i++) {
+//     for (let j = 0; j <= i; j++) {
+//         figure += '*';
+//     }
+//     figure += '\n';
+// }
+
+// console.log(figure);
+
+
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+
+
+
+
+// const lines = 5;
+// let result = '';
+// // Проверяется именно переменная result, формируйте строку в ней
+
+// for (let i = 0; i <= lines; i++) {
+
+//     for (let j = 1; j <= lines - i; j++) {
+//         result += ' ';
+//     }
+
+//     for (let j = 0; j <= 2 * i; j++) {
+//         result += '*';
+//     }
+
+//     result += '\n';
+// }
+
+// console.log(result);
 
